498c3cabf9a5b5eb42f3e3b99598229cb12db2ad
Bevan Lewis <blew0009@student.monash.edu>
2023-09-24 22:49:34 +1000

    redone raw files

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,5 +1,4 @@
-let vg_1 =
-  "https://raw.githubusercontent.com/bevanlewis/FIT3179_homework_w9/main/js/choropleth_map.vg.json";
+let vg_1 = "js/choropleth_map.vg.json";
 vegaEmbed("#choropleth_map", vg_1)
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
